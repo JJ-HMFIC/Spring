2b9ad8cd2bb0e5c82874dc997ef7437f88fb6696
KyungHwan Jeon <crasheso@gmail.com>
2024-09-30 17:06:14 +0900

    스프링 복습

---
+++ main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private final int discountFixAmount = 1000;// 1000원 할인
+    @Override
+    public int disount(Member member, int price) { //vip면 1000원 할인 , 아니면 0 반환
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+
+    }
+}
---
+++ test/java/hello/core/Beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,35 @@
+package hello.core.Beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + "// object = " + bean);;
+        }
+    } @Test
+    @DisplayName("애플리케이션 빈 출력")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) { // 빈이 사용자가 정의한 빈이면 출력
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + "// object = " + bean);;
+            }
+        }
+
+
+    }
+}
