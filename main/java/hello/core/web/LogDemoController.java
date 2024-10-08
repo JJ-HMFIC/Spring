c2000775b6dee5e4a94734a3f973d15e1e1b87ac
KyungHwan Jeon <crasheso@gmail.com>
2024-10-08 15:13:55 +0900

    proxy added

--- main/java/hello/core/web/LogDemoController.java
+++ main/java/hello/core/web/LogDemoController.java
@@ -5,6 +5,7 @@ import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,14 +16,19 @@ import org.springframework.web.client.HttpServerErrorException;
 public class LogDemoController {
     private final LogDemoService logDemoService;
     private final MyLogger myLogger;
+    //private final ObjectProvider<MyLogger> myLoggerObjectProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        //MyLogger myLogger = myLoggerObjectProvider.getObject();
         String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        Thread.sleep(100);
         logDemoService.logic("testid");
 
         return "OK";
